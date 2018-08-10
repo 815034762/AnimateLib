370a0739d5087b0179f51aa50fe774ef1b18a1f6
zhangtianyang <zhangty@healthmall.cn>
2018-08-10 18:02:05 +0800

    从左到右的动画实现

--- animationlibrary/src/main/java/cn/com/animationlibrary/AnimateTextView.java
+++ animationlibrary/src/main/java/cn/com/animationlibrary/AnimateTextView.java
@@ -1,6 +1,7 @@
 package cn.com.animationlibrary;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.support.annotation.AnimRes;
 import android.support.annotation.Nullable;
@@ -14,10 +15,9 @@ import android.view.animation.AnimationUtils;
 public class AnimateTextView extends android.support.v7.widget.AppCompatTextView {
 
     private Animation animation;
-    private MyRunnable runnable;
     private String content = "这是我在代码里面设置的内容";
     private int animateRes = 0;
-    private long delayTime = 2000;
+    private long duration;
     private boolean isStarted = false;
 
     public AnimateTextView(Context context) {
@@ -26,45 +26,39 @@ public class AnimateTextView extends android.support.v7.widget.AppCompatTextView
 
     public AnimateTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        runnable = new MyRunnable();
+        getAttribute(context, attrs);
     }
 
     public AnimateTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        getAttribute(context,attrs);
+    }
+
+    private void getAttribute(Context context, @Nullable AttributeSet attrs){
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AnimateTextView);
+        duration = typedArray.getInteger(R.styleable.AnimateTextView_animaDuration, 1500);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-    }
-
-    public void start() {
-        isStarted = true;
-        post(runnable);
-    }
-
-    public void stop() {
-        isStarted = false;
-        removeCallbacks(runnable);
-//        setText(content);
-//        setVisibility(VISIBLE);
-    }
-
-    public void setAnimations(@AnimRes int animRes) {
-        animateRes = animRes;
-        this.animation = AnimationUtils.loadAnimation(getContext(), animRes);
+        //animation 不生效，必须根据Res重新生成，好奇怪
+        setAnimations(animateRes);
+        animation.setDuration(duration);
+        setAnimation(animation);
 
         animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
 
-                AnimateTextView.this.setVisibility(VISIBLE);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
 
-                AnimateTextView.this.setVisibility(INVISIBLE);
+                if(isStarted){
+                    invalidate();
+                }
             }
 
             @Override
@@ -72,28 +66,27 @@ public class AnimateTextView extends android.support.v7.widget.AppCompatTextView
 
             }
         });
+    }
 
+    public void start() {
+
+        isStarted = true;
+        invalidate();
+    }
+
+    public void stop() {
+        isStarted = false;
+    }
+
+    public void setAnimations(@AnimRes int animRes) {
+        animateRes = animRes;
+        this.animation = AnimationUtils.loadAnimation(getContext(), animRes);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         isStarted = false;
-        removeCallbacks(runnable);
-    }
-
-    public class MyRunnable implements Runnable {
-
-        @Override
-        public void run() {
-            if (null != animation) {
-                //animation 不生效，必须根据Res重新生成，好奇怪
-                setAnimations(R.anim.right_to_left);
-                setAnimation(animation);
-                invalidate();
-                postDelayed(this, delayTime);
-            }
-        }
     }
 
     public boolean isStarted() {
